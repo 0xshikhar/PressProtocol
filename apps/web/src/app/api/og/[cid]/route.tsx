f1d281575f70804fa2753aebedbe4d49b6d631b9
0xshikhar <64684053+0xshikhar@users.noreply.github.com>
2026-09-24 20:05:31 +0530

    update : OG API route

--- apps/web/src/app/api/og/[cid]/route.tsx
+++ apps/web/src/app/api/og/[cid]/route.tsx
@@ -18,6 +18,7 @@ export async function GET(
   { params }: { params: { cid: string } }
 ) {
   const { cid } = params;
+  const { searchParams } = request.nextUrl;
   const rawQuote = searchParams.get("q") || searchParams.get("quote") || "";
   const quote = rawQuote.replace(/\+/g, " ").trim();
 
